708226baeaf93640e0358522a97870bc77aa917b
Justin Welham <jwelham@BCAA.AE>
2019-10-15 18:03:42 +0400

    long coding input

--- reversi.js
+++ reversi.js
@@ -69,16 +69,22 @@ var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
 myCanvas.addEventListener('click', function(event) {
+    
     var rect = myCanvas.getBoundingClientRect();
     var x = event.clientX - rect.left;
     var y = event.clientY - rect.top;
+    /* LOOP FUNCTION MISFIRES AFTER A WHILE
     if(gameRun){
+        console.log("before loop x:" + x + " y:" + y);
         for (g=0; g<8; g++){
             for (f=0; f<8; f++){
                 if (whiteTurn){
                     //add a loop to check and add new piece
-                    if(x>((f*50)+20) && x <((f*50))+70 && y>((g*50)+20) && y <((g*50)+70) && board[f][g]== "."){
+                    if(x>((f*50)+20) && x <((f*50))+70 && y>((g*50)+20) && y <((g*50)+70) && board[g][f]== "."){
+                        console.log(board[g][f]);
                         board[g][f] = "W";
+                        console.log("x: " + x + " y: " + y);
+                        console.log("g: " + g + " f: " + f);
                         drawBoard();
                         whiteTurn = false;
                         document.getElementById('turnIndicator').innerHTML = "Black Turn";
@@ -87,7 +93,10 @@ myCanvas.addEventListener('click', function(event) {
                 } else {
                     //add black tokens
                     if(x>((f*50)+20) && x <((f*50))+70 && y>((g*50)+20) && y <((g*50)+70) && board[f][g]== "."){
+                        console.log(board[g][f]);
                         board[g][f] = "B";
+                        console.log("x: " + x + " y: " + y);
+                        console.log("g: " + g + " f: " + f);
                         drawBoard();
                         whiteTurn = true;
                         document.getElementById('turnIndicator').innerHTML = "White Turn";
@@ -96,8 +105,218 @@ myCanvas.addEventListener('click', function(event) {
                 }
             }
         }
-    }
+    }*/
+    //LONGHAND THE FUNCTION
+    
+    if(gameRun){
+        
+        if(whiteTurn){//TOP ROW
+            if(x>20 && x< 70 && y > 20 && y < 70 && board[0][0] == "."){
+                board[0][0] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            
+            }
+            else if(x>70 && x< 120 && y > 20 && y < 70 && board[0][1] == "."){
+                board[0][1] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+            else if(x>120 && x< 170 && y > 20 && y < 70 && board[0][2] == "."){
+                board[0][2] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+            else if(x>170 && x< 220 && y > 20 && y < 70 && board[0][3] == "."){
+                board[0][3] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+            else if(x>220 && x< 270 && y > 20 && y < 70 && board[0][4] == "."){
+                board[0][4] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+            else if(x>270 && x< 320 && y > 20 && y < 70 && board[0][5] == "."){
+                board[0][5] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+            else if(x>320 && x< 370 && y > 20 && y < 70 && board[0][6] == "."){
+                board[0][6] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+            else if(x>370 && x< 420 && y > 20 && y < 70 && board[0][7] == "."){
+                board[0][7] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }//SECOND ROW
 
+            else if(x>20 && x< 70 && y > 70 && y < 120 && board[1][0] == "."){
+                board[1][0] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            
+            }
+            else if(x>70 && x< 120 && y > 70 && y < 120 && board[1][1] == "."){
+                board[1][1] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+            else if(x>120 && x< 170 && y > 70 && y < 120 && board[1][2] == "."){
+                board[1][2] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+            else if(x>170 && x< 220 && y > 70 && y < 120 && board[1][3] == "."){
+                board[1][3] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+            else if(x>220 && x< 270 && y > 70 && y < 120 && board[1][4] == "."){
+                board[1][4] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+            else if(x>270 && x< 320 && y > 70 && y < 120 && board[1][5] == "."){
+                board[1][5] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+            else if(x>320 && x< 370 && y > 70 && y < 120 && board[1][6] == "."){
+                board[1][6] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+            else if(x>370 && x< 420 && y > 70 && y < 120 && board[1][7] == "."){
+                board[1][7] = "W";
+                drawBoard();
+                whiteTurn = false;
+                document.getElementById("turnIndicator").innerHTML = "Black Turn";
+            }
+
+
+
+        } else {//BLACK TOKENS
+            if(x>20 && x< 70 && y > 20 && y < 70 && board[0][0] == "."){
+                board[0][0] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>70 && x< 120 && y > 20 && y < 70 && board[0][1] == "."){
+                board[0][1] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>120 && x< 170 && y > 20 && y < 70 && board[0][2] == "."){
+                board[0][2] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>170 && x< 220 && y > 20 && y < 70 && board[0][3] == "."){
+                board[0][3] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>220 && x< 270 && y > 20 && y < 70 && board[0][4] == "."){
+                board[0][4] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>270 && x< 320 && y > 20 && y < 70 && board[0][5] == "."){
+                board[0][5] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>320 && x< 370 && y > 20 && y < 70 && board[0][6] == "."){
+                board[0][6] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>370 && x< 420 && y > 20 && y < 70 && board[0][7] == "."){
+                board[0][7] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            //SECOND ROW
+            
+            else if(x>20 && x< 70 && y > 70 && y < 120 && board[1][0] == "."){
+                board[1][0] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            
+            }
+            else if(x>70 && x< 120 && y > 70 && y < 120 && board[1][1] == "."){
+                board[1][1] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>120 && x< 170 && y > 70 && y < 120 && board[1][2] == "."){
+                board[1][2] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>170 && x< 220 && y > 70 && y < 120 && board[1][3] == "."){
+                board[1][3] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>220 && x< 270 && y > 70 && y < 120 && board[1][4] == "."){
+                board[1][4] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>270 && x< 320 && y > 70 && y < 120 && board[1][5] == "."){
+                board[1][5] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>320 && x< 370 && y > 70 && y < 120 && board[1][6] == "."){
+                board[1][6] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+            else if(x>370 && x< 420 && y > 70 && y < 120 && board[1][7] == "."){
+                board[1][7] = "B";
+                drawBoard();
+                whiteTurn = true;
+                document.getElementById("turnIndicator").innerHTML = "White Turn";
+            }
+
+        }
+    }
+    //console.log("x:" + x + " y:" + y);
 }, false);
 
 drawBoard();
